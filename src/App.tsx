d376fd9bd0ef1b3261006cbae8a489a4b901850c
30parth <aparth3010@gmail.com>
2025-08-19 21:51:00 +0530

    add supabase

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,44 @@
-import './App.css'
+import './App.css';
+import { useState, useEffect } from 'react';
+import supabase from './utils/supabase';
+
+type Todo = {
+  id: number;
+  user_name: string;
+  // Add other fields as required
+};
+type TodoInsert = Omit<Todo, "id">;
 
 function App() {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  useEffect(() => {
+    const getTodos = async () => {
+      const { data: todos, error } = await supabase
+        .from<Todo, TodoInsert>("user")
+        .select();
+
+      if (error) {
+        console.error("Error fetching todos:", error);
+        return;
+      }
+      if (todos && todos.length > 1) {
+        setTodos(todos);
+      }
+    };
+
+    getTodos();
+  }, []);
 
   return (
     <>
-      <h1>
-        Hii I am Parth 
-      </h1>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.user_name}</li>
+        ))}
+      </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
